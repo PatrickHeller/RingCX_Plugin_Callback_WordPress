17617ab0da53635da370950ad9dcec9c5078b20d
Patrick Heller <hellerpat@gmail.com>
2026-05-02 14:09:25 +0200

    Create callback4ringcx.php

---
+++ callback4ringcx.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Plugin Name: CallBack4RingCX
+ * Description: Floating Callback Button für WordPress mit Übergabe an RingCX Voice als Lead/Callback.
+ * Version: 1.0.1
+ * Author: Patrick Heller
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! function_exists( 'callback4ringcx_log' ) ) {
+	/**
+	 * Debug logger.
+	 *
+	 * @param mixed $message Message to log.
+	 * @return void
+	 */
+	function callback4ringcx_log( $message ) {
+		if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+			if ( is_array( $message ) || is_object( $message ) ) {
+				error_log( print_r( $message, true ) );
+			} else {
+				error_log( $message );
+			}
+		}
+	}
+}
+
+define( 'CALLBACK4RINGCX_VERSION', '1.0.1' );
+define( 'CALLBACK4RINGCX_FILE', __FILE__ );
+define( 'CALLBACK4RINGCX_PATH', plugin_dir_path( __FILE__ ) );
+define( 'CALLBACK4RINGCX_URL', plugin_dir_url( __FILE__ ) );
+
+require_once CALLBACK4RINGCX_PATH . 'includes/class-callback4ringcx-settings.php';
+require_once CALLBACK4RINGCX_PATH . 'includes/class-callback4ringcx-api.php';
+require_once CALLBACK4RINGCX_PATH . 'admin/class-callback4ringcx-admin.php';
+require_once CALLBACK4RINGCX_PATH . 'public/class-callback4ringcx-public.php';
+require_once CALLBACK4RINGCX_PATH . 'public/class-callback4ringcx-ajax.php';
+require_once CALLBACK4RINGCX_PATH . 'includes/class-callback4ringcx.php';
+
+/**
+ * Run plugin.
+ *
+ * @return void
+ */
+function callback4ringcx_run() {
+	$plugin = new CallBack4RingCX();
+	$plugin->run();
+}
+
+callback4ringcx_run();
