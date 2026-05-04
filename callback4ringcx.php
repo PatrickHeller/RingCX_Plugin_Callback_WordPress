09c28494db983a8308387129cbfafaea9648d414
Patrick Heller <hellerpat@gmail.com>
2026-05-04 14:38:05 +0200

    Update callback4ringcx.php

--- callback4ringcx.php
+++ callback4ringcx.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: CallBack4RingCX
  * Description: Floating Callback Button für WordPress mit Übergabe an RingCX Voice als Lead/Callback.
- * Version: 1.0.10
+ * Version: 1.1.0
  * Author: Patrick Heller
  * GitHub Plugin URI: https://github.com/PatrickHeller/WordPress-CallBack4RingCX
  * Primary Branch: main
